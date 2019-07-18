267d271f2241e4adce74c0c5421223d818f097d2
derden2000 <derden2000@mail.ru>
2019-07-19 02:42:13 +0300

    Initial commit

---
+++ Trainings/src/ru/antonshu/Alg2/CustomArr.java
@@ -0,0 +1,55 @@
+package ru.antonshu.Alg2;
+
+public class CustomArr {
+//    private int[] arr;
+//
+//    public CustomArr(int size){
+//        this.arr = new int[size];
+//        for (int i = 0; i < size; i++) {
+//            this.arr[i] = (int) Math.random() * size;
+//        }
+//    }
+//
+//    public void swap(int firstIndex, int secondIndex) {
+//        int num = this.arr[firstIndex];
+//        this.arr[firstIndex] = this.arr[secondIndex];
+//        this.arr[secondIndex] = num;
+//    }
+//
+//    public void bubbleSort() {
+//        for (int external = this.arr.length-1; external >= 1 ; external--) {
+//            for (int internal = 0; internal < external; internal++) {
+//                if (arr[internal] > arr[internal+1]) {
+//                    swap(internal, internal+1);
+//                }
+//            }
+//        }
+//    }
+//
+//    public void selectSort() {
+//        int internal, external, marker;
+//        for(external = 0; external < this.arr.length; external++){
+//            marker = external;
+//            for(internal = external+1; internal < this.arr.length; internal++){
+//                if (this.arr[internal] < this.arr[marker]){
+//                    marker = internal;
+//                }
+//            }
+//            swap(external, marker);
+//        }
+//    }
+//
+//    public void insertSort() {
+//        int internal, external;
+//        for(external = 1; external < this.arr.length; external++){
+//            int num = this.arr[external];
+//            internal = external;
+//            while(internal > 0 && this.arr[internal-1] >= num){
+//                this.arr[internal] = this.arr[internal-1];
+//                --internal;
+//            }
+//            this.arr[internal] = num;
+//        }
+//    }
+
+}
---
+++ Trainings/src/ru/antonshu/Alg2/SortDiffs.java
@@ -0,0 +1,5 @@
+package ru.antonshu.Alg2;
+
+public class SortDiffs {
+
+}
