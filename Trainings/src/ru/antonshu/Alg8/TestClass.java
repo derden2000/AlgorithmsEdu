371da3837efab747f0bd43602ef9639aa5aff001
antonshu <derden2000@mail.ru>
2019-08-09 17:15:31 +0300

    Sample of MyHashMap

---
+++ Trainings/src/ru/antonshu/Alg8/TestClass.java
@@ -0,0 +1,19 @@
+package ru.antonshu.Alg8;
+
+public class TestClass {
+    public static void main(String[] args) {
+        ChainingHashMap<Integer, String> myHashMap = new ChainingHashMap<>();
+
+        myHashMap.put(1, "One");
+        myHashMap.put(2, "Two");
+        myHashMap.put(3, "Three");
+        myHashMap.put(4, "Four");
+        myHashMap.put(4, "Five");
+
+        System.out.println("myHashMap: \n" + myHashMap);
+
+        myHashMap.delete(2);
+        System.out.println("myHashMap: \n" + myHashMap);
+
+    }
+}
